7fd9ee7d53bb9227783ef21c07008541587df0df
Amuthan <mech.amuthansakthivel@gmail.com>
2021-11-17 18:22:33 +0530

    Added page and page component

---
+++ src/main/java/com/assignment/pages/HomePage.java
@@ -0,0 +1,17 @@
+package com.assignment.pages;
+
+import com.assignment.pages.pagecomponents.TopMenuComponent;
+
+public class HomePage {
+
+    private final TopMenuComponent topMenuComponent;
+
+    private HomePage(){
+        topMenuComponent = new TopMenuComponent();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/assignment/pages/pagecomponents/TopMenuComponent.java
@@ -0,0 +1,14 @@
+package com.assignment.pages.pagecomponents;
+
+import org.openqa.selenium.By;
+
+public class TopMenuComponent {
+
+    By txtboxSearch = By.id("twotabsearchtextbox");
+
+    public TopMenuComponent setTextInSearch(){
+
+        return this;
+    }
+
+}
