85f86c41cc7bf89de20cd2f302510264bccffa79
Amuthan <mech.amuthansakthivel@gmail.com>
2021-11-17 18:43:53 +0530

    Added SearchBy Component

---
+++ src/main/java/com/assignment/pages/pagecomponents/SortByComponent.java
@@ -0,0 +1,9 @@
+package com.assignment.pages.pagecomponents;
+
+import org.openqa.selenium.By;
+
+public final class SortByComponent {
+
+    private final By drpdwnSort = By.xpath("//span[text()='Sort by:']");
+    private final By dropdwnOption = By.xpath("Price: High to Low");
+}
