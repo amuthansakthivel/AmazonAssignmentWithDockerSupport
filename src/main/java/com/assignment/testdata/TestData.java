635eda93047ac4c6fde4dccdb1d42426724cd00e
Amuthan <mech.amuthansakthivel@gmail.com>
2021-11-18 09:32:01 +0530

    Added TestData Pojo and dataprovider

---
+++ src/main/java/com/assignment/testdata/TestData.java
@@ -0,0 +1,13 @@
+package com.assignment.testdata;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder(setterPrefix = "set")
+@Getter
+public class TestData {
+
+    private String itemName;
+    private int quantity;
+}
--- src/test/java/com/assignment/tests/AddToCartTests.java
+++ src/test/java/com/assignment/tests/AddToCartTests.java
@@ -3,10 +3,12 @@ package com.assignment.tests;
 import com.assignment.driver.Driver;
 import com.assignment.facade.CheckoutFacade;
 import com.assignment.facade.SearchFacade;
+import com.assignment.testdata.TestData;
 import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AddToCartTests {
@@ -21,19 +23,25 @@ public class AddToCartTests {
         Driver.getInstance().tearDownDriver();
     }
 
-    @SneakyThrows
-    @Test(description = "To check whether the user can add cheapest chocolate to the cart and verify the total cost")
-    public void addToCartTest() {
-        Double priceOfOneItem = new SearchFacade().searchItem("Ferroro Rocher")
-                .addProductWithMentionedQtyToCart(2)
+    @Test(dataProvider = "getData", description = "To check whether the user can add cheapest chocolate to the cart and verify the total cost")
+    public void verifyTotalCartPriceInCheckoutPageTest(TestData testData) {
+
+        Double priceOfOneItem = new SearchFacade().searchItem(testData.getItemName())
+                .addProductWithMentionedQtyToCart(testData.getQuantity())
                 .getPriceOfOneItem();
 
         Double totalCartValue = new CheckoutFacade().getTotalCartValue();
 
         Assertions.assertThat(totalCartValue)
                         .isPositive()
-                        .isEqualTo(priceOfOneItem*2);
+                        .isEqualTo(priceOfOneItem * testData.getQuantity());
+    }
 
+    @DataProvider
+    public Object[][] getData(){
+        return new Object[][]{
+                {TestData.builder().setItemName("Ferroro Rocher").setQuantity(2).build()}
+        };
     }
 
 
